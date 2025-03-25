557d564a740d63d25dd630a72ff13e55744f34a7
Daryl <164248137+darealghost7@users.noreply.github.com>
2025-03-25 15:59:58 +0200

    Add files via upload

---
+++ stringfiles.py
@@ -0,0 +1,21 @@
+#name = input("enter your name: ")
+#name = name.upper
+#result = name.find("o")
+#result = name.isdigit()#it only is true if there is digits
+#result = phone_number.count(" - ")
+#phone_number.replace(" - " " , ")
+#print(help(str))
+#len()
+#print(result)
+
+#indexing  = accessing elements of a sequence using []
+#                   [start : end : step]
+
+credit_number = "1234-5678-9012-3456"
+
+#print(credit_number[0])
+#print(credit_number [:4])#the first four no.
+#print(credit_number [5:9])
+#print(credit_number [5:])#from 5 to the end
+#print(credit_number [-1])#start from the last no.
+print(credit_number [::2])#counting in 2's
